ff6d891afe68869feb542fd0e70294ec2ff8ab0c
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-08-23 00:19:42 +0530

    feat(backend): implement User controller with DB save logic & Nodemailer

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,5 +1,73 @@
+import User from "../model/User.model.js";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
+
 const registerUser = async (req, res) => {
-  res.send("Registered");
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      message: "All fields are requires",
+    });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not registered",
+      });
+    }
+
+    const token = crypto.randomBytes(32).toString("hex");
+    user.verificationToken = token;
+
+    await user.save();
+
+    // send email
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: process.env.MAILTRAP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.MAILTRAP_USERNAME,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    });
+
+    const mailOption = {
+      from: process.env.MAILTRAP_SENDEREMAIL,
+      to: user.email,
+      subject: "Verify ✔ your email",
+      text: `Please click on the following link:
+      ${process.env.BASE_URL}/api/v1/users/register/${token}
+      `,
+    };
+
+    await transporter.sendMail(mailOption);
+
+    res.status(200).json({
+      message: "User registered successfully",
+      success: true,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "User not registered",
+      err,
+      success: false,
+    });
+  }
 };
 
 export default registerUser;
