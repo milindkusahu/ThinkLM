f1415213d5747fd0acbea2137500cc540735823d
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-08-29 17:07:12 +0530

    feat(backend): implement user profile, stats, password change & account deletion routes in user.routes.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getUserStats,
   forgotPassword,
   getProfile,
   login,
@@ -7,6 +8,9 @@ import {
   registerUser,
   resetPassword,
   verifyUser,
+  updateProfile,
+  changePassword,
+  deleteAccount,
 } from "../controller/user.controller.js";
 import isLoggedIn from "../middleware/auth.middleware.js";
 
@@ -15,9 +19,16 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.get("/verify/:token", verifyUser);
 router.post("/login", login);
+
 router.get("/profile", isLoggedIn, getProfile);
+router.put("/profile", isLoggedIn, updateProfile);
+router.get("/stats", isLoggedIn, getUserStats);
 router.get("/logout", isLoggedIn, logout);
+
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
+
+router.post("/change-password", isLoggedIn, changePassword);
+router.delete("/delete-account", isLoggedIn, deleteAccount);
 
 export default router;
